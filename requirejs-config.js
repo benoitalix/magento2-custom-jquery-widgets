a3fc9341a407ba7778f74d8996df7cd46f95206e
Benoit Alix <benoit.alix@gmail.com>
2020-04-17 23:03:50 +0200

    Update requirejs config

---
+++ requirejs-config.js
@@ -0,0 +1,13 @@
+var config = {
+    map: {
+        '*': {
+            categoryFilter:'js/category-filter',
+            inputFileCustom:'js/input-file-custom',
+            listToSelect:'js/list-to-select',
+            passwordShow:'js/password-show',
+            passwordStrength:'js/password-strength',
+            scrollTo:'js/scroll-to',
+            categoryFilter:'js/sticky-header'
+        }
+    }
+};
